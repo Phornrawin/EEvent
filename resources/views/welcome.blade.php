90c7cbdfcd7150025ec15c6e50461ceada24405a
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2018-03-08 11:14:22 +0700

    insert img to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="container">
-        <div class="d-flex flex-column text-center justify-content-center pb-xl-5" style="height: 100vh">
+        <div class="d-flex flex-column text-center justify-content-center pb-xl-5" style="height: 100vh;background-image: url(http://daxushequ.com/data/out/48/img60583455.jpg)">
             <div>
-                <h1 class="display-1">EEvent</h1>
-                <p>[ Where Everything meets Everywhere ]</>
+                <h1 class="display-1" style="color: white">EEvent</h1>
+                <p style="color: white">[ Where Everything meets Everywhere ]</p>
                 <div>
                     <a class="btn btn-success m-2" href={{route('login')}}>Join Us</a>
                 </div>
@@ -16,5 +16,5 @@
                 <input type="text" class="form-text">
             </form>
         </div>
-    </div>
+    </div>   
 @endsection
