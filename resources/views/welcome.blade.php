bf3cb16b87cd0d86dfbb1aa58bb9323ace1808ce
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2018-03-08 11:37:04 +0700

    recommit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,8 @@
     <div class="container">
         <div class="d-flex flex-column text-center justify-content-center pb-xl-5" style="height: 100vh;background-image: url(http://daxushequ.com/data/out/48/img60583455.jpg)">
             <div>
-<<<<<<< HEAD
                 <h1 class="display-1" style="color: white">EEvent</h1>
                 <p style="color: white">[ Where Everything meets Everywhere ]</p>
-=======
-                <h1 class="display-1">EEvent</h1>
-                <p>[ Where Everything meets Everywhere ]</p>
->>>>>>> 0a1a2095926c21b2dcb2bef79b1159e108ed380c
                 <div>
                     <a class="btn btn-success m-2" href={{route('login')}}>Join Us</a>
                 </div>
@@ -18,6 +13,7 @@
         </div>
         <div class="d-flex jumbotron justify-content-center">
             <form class="">
+                <p>search by:</p>
                 <input type="text" class="form-text">
             </form>
         </div>
