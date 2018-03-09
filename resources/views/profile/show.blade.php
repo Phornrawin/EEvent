e5a213fe038e069004cf2514deb6ec7f0a4c9ed7
shraco <nate.t@ku.th>
2018-03-09 12:00:23 +0700

    edit table event

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -17,33 +17,62 @@
                     <button type="submit">ok</button>
                 </form>
                 <br><br><br>
+                @if(!$user->attendEvent->isEmpty())
+                    <h3>Event ที่เคยเข้าร่วม</h3>
+                    <table style="width:100% ; border: 4px solid #34ce57">
+                        <tr style="width:100% ; border: 1px solid #34ce57">
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th>Start Time</th>
+                            <th>End Time</th>
+                        </tr>
+                        <tr>
+                            <td>@foreach($user->attendEvent as $role)
+                                    <div><a href="#" >{{$role->name}}</a></div>
+                                @endforeach
+                            </td>
+                            <td>@foreach($user->attendEvent as $role)
+                                    <div>{{$role->location}}</div>
+                                @endforeach</td>
+                            <td>@foreach($user->attendEvent as $role)
+                                    <div>{{$role->start_time}}</div>
+                                @endforeach</td>
+                            <td>@foreach($user->attendEvent as $role)
+                                    <div>{{$role->end_time}}</div>
+                                @endforeach</td>
+                        </tr>
 
-                <h3>Event ที่เคยเข้าร่วม</h3>
+                    </table>
+                @elseif(!$user->organizedEvent->isEmpty())
+                    <h3>Event ที่จัด</h3>
 
-                <table style="width:100% ; border: 4px solid #34ce57">
-                    <tr style="width:100% ; border: 1px solid #34ce57">
-                        <th>Name</th>
-                        <th>Location</th>
-                        <th>Start Time</th>
-                        <th>End Time</th>
-                    </tr>
-                    <tr>
-                        <td>@foreach($user->attendEvent as $role)
-                                <div><a href="#" >{{$role->name}}</a></div>
-                            @endforeach
-                        </td>
-                        <td>@foreach($user->attendEvent as $role)
-                                <div>{{$role->location}}</div>
-                            @endforeach</td>
-                        <td>@foreach($user->attendEvent as $role)
-                                <div>{{$role->start_time}}</div>
-                            @endforeach</td>
-                        <td>@foreach($user->attendEvent as $role)
-                                <div>{{$role->end_time}}</div>
-                            @endforeach</td>
-                    </tr>
+                    <table style="width:100% ; border: 4px solid #34ce57">
+                        <tr style="width:100% ; border: 1px solid #34ce57">
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th>Start Time</th>
+                            <th>End Time</th>
+                        </tr>
+                        <tr>
+                            <td>@foreach($user->organizedEvent as $role)
+                                    <div><a href="#" >{{$role->name}}</a></div>
+                                @endforeach
+                            </td>
+                            <td>@foreach($user->organizedEvent as $role)
+                                    <div>{{$role->location}}</div>
+                                @endforeach</td>
+                            <td>@foreach($user->organizedEvent as $role)
+                                    <div>{{$role->start_time}}</div>
+                                @endforeach</td>
+                            <td>@foreach($user->organizedEvent as $role)
+                                    <div>{{$role->end_time}}</div>
+                                @endforeach</td>
+                        </tr>
 
-                </table>
+                    </table>
+                @endif
+
+
 
             </div>
         </div>
