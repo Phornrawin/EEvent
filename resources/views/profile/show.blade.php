ec0e878aa32aea3b3d9564ff21b2415ba5d0ebf1
shraco <nate.t@ku.th>
2018-03-09 11:25:01 +0700

    edit table event

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,6 +7,7 @@
             <div class="col-md-10">
                 <img src="uploads/avatars/{{$user->avatar }}"
                      style="width: 150px; height: 150px; float: left; border-radius: 50%;margin-right: 20px">
+
                 <h2>Hi! {{$user->name}}</h2>
                 <h3>Email: {{$user->email}}</h3>
                 <form enctype="multipart/form-data" action="{{route('profile')}}" method="POST">
@@ -16,15 +17,30 @@
                     <button type="submit">ok</button>
                 </form>
                 <br><br><br>
+
                 <h3>Event ที่เคยเข้าร่วม</h3>
+
                 <table style="width:100% ; border: 4px solid #34ce57">
                     <tr style="width:100% ; border: 1px solid #34ce57">
                         <th>Name</th>
                         <th>Location</th>
+                        <th>Start Time</th>
+                        <th>End Time</th>
                     </tr>
                     <tr>
-                        <td>event</td>
-                        <td>location event</td>
+                        <td>@foreach($user->attendEvent as $role)
+                                <div><a href="#" >{{$role->name}}</a></div>
+                            @endforeach
+                        </td>
+                        <td>@foreach($user->attendEvent as $role)
+                                <div>{{$role->location}}</div>
+                            @endforeach</td>
+                        <td>@foreach($user->attendEvent as $role)
+                                <div>{{$role->start_time}}</div>
+                            @endforeach</td>
+                        <td>@foreach($user->attendEvent as $role)
+                                <div>{{$role->end_time}}</div>
+                            @endforeach</td>
                     </tr>
 
                 </table>
