54e3a843a5e462f4be0e4bec7ae18d3ee7700de5
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2018-03-11 23:10:46 +0700

    build Admin Create Event complet

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -37,7 +37,7 @@
 					</div>
     			</div>
 
-    			<div class="col-md-10" align="center">
+    			<div class="col-md-10" align="center" style="width: 100%">
     				 <div id="users" class="content">
 				        <table class="table">
 				            <tr class="table table-dark">
@@ -114,88 +114,98 @@
 				    </div>
 
 				    <div id="createEvent" class="content">
-				       <div class="container">
-				        <div class="row">
-				            <form method="post" class="contact1-form validate-form" action="{{route('admin.events.store')}}">
-            					@csrf
-				                <div class="row">
-				                    <label>Event name:<input type="text" name="name" placeholder="Name" class="form-control"></label>
-				                    @if($errors->has('name'))
-				                        <span class="help-block">{{ $errors->first('name') }}</span>
-				                    @endif
+				        	<div class="row" >
+					            <form method="post" class="contact1-form validate-form" action="{{route('admin.events.store')}}" style="width: 80%" align="center">
+	            					@csrf
+	            					<h3>Create Event</h3>
+	            					<hr style="height: 2px; color:#1C2833; background:#1C2833;">
+	            					<div class="row"> <!-- first row -->
+	            						<div class="col-md-6">
+	            						 <label >Event name:<input type="text" name="name" placeholder="Name" class="form-control" style="width: 100%" size="300" ></label>
+						                    @if($errors->has('name'))
+						                        <span class="help-block">{{ $errors->first('name') }}</span>
+						                    @endif
+	            						</div>
+		            					<div class="col-md-3">
+		            						<label>Category
+					                            <div class="input-group mb-3">
+					                                <div class="input-group-prepend">
+					                                    <label class="input-group-text" for="inputGroupSelect01">Category</label>
+					                                </div>
+					                                <select class="custom-select" id="category_selector" name="category_id">
+					                                    <option value="" selected disabled hidden>Select category</option>
+					                                    <option value="1">Adventure</option>
+					                                    <option value="2">Dance</option>
+					                                    <option value="3">Food</option>
+					                                    <option value="4">Movement</option>
+					                                    <option value="5">Movie</option>
+					                                    <option value="6">Other</option>
+					                                </select>
+					                            </div>
+					                    	</label>
+						                    @if($errors->has('category'))
+						                        <span class="help-block">{{ $errors->first('category') }}</span>
+						                    @endif
+		            					</div>
+		            					<div class="col-md-3">
+		            						<label>Maximum Attendee:<input type="number" name="max_capacity" placeholder="Attendee number"
+                                                   min="0" class="form-control"></label>
+						                    @if($errors->has('max_capacity'))
+						                        <span class="help-block">{{ $errors->first('max_capacity') }}</span>
+						                    @endif
+								        </div>
+	            					</div>
 
+	            					<div class="row"> <!-- second row -->
+	            						<div class="col-md-3">
+	            							<label>Event fee (Baht) <input type="float" name="price" placeholder="0" class="form-control"></label>
+						                    @if($errors->has('price'))
+						                        <span class="help-block">{{ $errors->first('price') }}</span>
+						                    @endif
+	            						</div>
+	            						<div class="col-md-3">
+	            							 <label>Pay fee before: <input type="datetime-local" name="payment_time"
+                                                  class="form-control"></label>
+						                    @if($errors->has('payment_time'))
+						                        <span class="help-block">{{ $errors->first('payment_time') }}</span>
+						                    @endif
+	            						</div>
+	            						<div class="col-md-3">
+	            							<label>Location <input type="text" name="location" placeholder="Location"
+                                           class="form-control"></label>
+						                    @if($errors->has('location'))
+						                        <span class="help-block">{{ $errors->first('location') }}</span>
+						                    @endif
+	            						</div>
+	            						<div class="col-md-3">
+	            							<label>Event start time: <input type="datetime-local" name="start_time"
+                                                    class="form-control"></label>
+						                    @if($errors->has('start_time'))
+						                        <span class="help-block">{{ $errors->first('start_time') }}</span>
+						                    @endif
+	            						</div>
+	            					</div>
 
-				                    <label>Category
-				                            <div class="input-group mb-3">
-				                                <div class="input-group-prepend">
-				                                    <label class="input-group-text" for="inputGroupSelect01">Category</label>
-				                                </div>
-				                                <select class="custom-select" id="category_selector" name="category_id">
-				                                    <option value="" selected disabled hidden>Select category</option>
-				                                    <option value="1">Adventure</option>
-				                                    <option value="2">Dance</option>
-				                                    <option value="3">Food</option>
-				                                    <option value="4">Movement</option>
-				                                    <option value="5">Movie</option>
-				                                    <option value="6">Other</option>
-				                                </select>
-				                            </div>
-				                    </label>
-				                    @if($errors->has('category'))
-				                        <span class="help-block">{{ $errors->first('category') }}</span>
-				                    @endif
+	            					<div class="col-md-12">
+					                    <label>Detail:<textarea rows="10" cols="80" name="detail" placeholder="Detail" class="form-control"></textarea></label>
+					                    @if($errors->has('detail'))
+					                        <span class="help-block">{{ $errors->first('detail') }}</span>
+					                    @endif
+	            					</div>
+	            					
+	            					<div class="col-md-12">
+	            						<input type="hidden" value="{{Auth::id()}}" name="organizer_id">
 
-				                </div>
-
-				                <div class="row">
-
-				                    <label>Maximum Attendee:<input type="number" name="max_capacity" placeholder="Attendee number"
-				                                                   min="0" class="form-control"></label>
-				                    @if($errors->has('max_capacity'))
-				                        <span class="help-block">{{ $errors->first('max_capacity') }}</span>
-				                    @endif
-				                    <label>Event fee (Baht) <input type="float" name="price" placeholder="0" class="form-control"></label>
-				                    @if($errors->has('price'))
-				                        <span class="help-block">{{ $errors->first('price') }}</span>
-				                    @endif
-
-				                    <label>Pay fee before: <input type="datetime-local" name="payment_time"
-				                                                  class="form-control"></label>
-				                    @if($errors->has('payment_time'))
-				                        <span class="help-block">{{ $errors->first('payment_time') }}</span>
-				                    @endif
-				                </div>
-				                <div class="row">
-				                    <label>Location <input type="text" name="location" placeholder="Location"
-				                                           class="form-control"></label>
-				                    @if($errors->has('location'))
-				                        <span class="help-block">{{ $errors->first('location') }}</span>
-				                    @endif
-
-
-				                    <label>Event start time: <input type="datetime-local" name="start_time"
-				                                                    class="form-control"></label>
-				                    @if($errors->has('start_time'))
-				                        <span class="help-block">{{ $errors->first('start_time') }}</span>
-				                    @endif
-				                </div>
-				                <div class="row">
-				                    <label>Detail:<textarea name="detail" placeholder="Detail" class="form-control"></textarea></label>
-				                    @if($errors->has('detail'))
-				                        <span class="help-block">{{ $errors->first('detail') }}</span>
-				                    @endif
-				                </div>
-				                <input type="hidden" value="{{Auth::id()}}" name="organizer_id">
-
-				                <button type="submit" class="btn btn-primary">Create!</button>
-				            </form>
-				            </div>
-				        </div> 
-   					 </div>
+               							 <button type="submit" class="btn btn-primary">Create!</button>
+	            					</div>
+	            					
+            					
+				            	</form>
+				     		</div>
+   					</div>
 
 
     			</div>
-    		
     	</div>
     </div>
     
