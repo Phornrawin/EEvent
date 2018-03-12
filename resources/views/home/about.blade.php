ba2ed6d68f9b3c5f9e83479cd08941c83c574c9e
shraco <nate.t@ku.th>
2018-03-12 21:40:54 +0700

    Add detail to about us page

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -3,7 +3,26 @@
 @section('title', 'About us - EEvent');
 
 @section('content')
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
-        <div class="display-3">This is page is for about us</div>
+    <div class="d-flex justify-content-center align-items-center" >
+        <div class="display-3" style="line-height: 100px;">
+            About Us
+        </div>
+    </div>
+    <br><br><br>
+    <div style="text-align: center;font-size: 20px">WHAT'S YOUR EVENT CHOICE? <br>
+        We have solutions for various kinds of event.
+        Please choose one that best describes yours.</div>
+    <br><br><br><br>
+    <div style="text-align: center;font-size: 20px">
+        This project is part of the 01418443 Web Technology & Web Services.</div>
+    <br><br>
+    <h2 style="text-align: center;">- Authorities -</h2>
+    <div style="text-align: center;font-size: 20px;letter-spacing: 3px">
+        <br>
+        Chatchapol Rasameluangon 5810404901<br><br>
+        Papada preedagorn 5810405126<br><br>
+        Thanadon Pakawatthippoyom 5810405037<br><br>
+        Phornrawin Chitsoonthorn 5810405185<br><br>
+        Nate Trakarntanasuk 5810405100<br><br>
     </div>
 @endsection
